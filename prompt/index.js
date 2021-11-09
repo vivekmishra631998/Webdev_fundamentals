4c49c0280913396cbbfab5504f2904f9fbf433e5
Vivek <vivekmishra631998@gmail.com>
2021-11-09 20:37:14 +0530

    commited

---
+++ prompt/index.js
@@ -0,0 +1,19 @@
+function func(id) {
+
+    
+    let name = prompt('Enter your name : ','Vivek Mishra')
+    document.getElementById('fullName').value = name
+    
+  
+    
+    let std = prompt('Enter your standard', 10)
+    document.getElementById('standard').value = std
+    
+
+  
+    let rollNo = prompt('Enter your Roll Number', 40)
+    document.getElementById('RollNumber').value = rollNo
+    
+
+
+}
