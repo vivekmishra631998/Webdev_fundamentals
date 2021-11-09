d1cdd80f054757b4a1d34b0fc68588d794a9a85a
Vivek <vivekmishra631998@gmail.com>
2021-11-09 19:16:46 +0530

    commited

---
+++ counter_app/index.js
@@ -0,0 +1,29 @@
+
+function increment(id) {
+   let sum =  document.getElementById('value').innerText
+   sum = parseInt(sum)
+   sum++
+   document.getElementById('value').innerText= sum
+    
+   
+}
+
+
+function decrement(id) {
+    let sum =  document.getElementById('value').innerText
+    sum = parseInt(sum)
+    sum--
+    document.getElementById('value').innerText= sum
+    
+}
+
+
+
+function sayHi(className) {
+    
+    alert('Vivek Mishra says Hii !')
+}
+
+
+
+
